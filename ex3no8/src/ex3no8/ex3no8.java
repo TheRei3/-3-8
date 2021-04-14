216af8fcd7b509733eba6520e7b63d92475560cf
admin <admin@DESKTOP-8L2IG92>
2021-04-15 05:42:36 +0900

    ex3no8

---
+++ ex3no8/src/ex3no8/ex3no8.java
@@ -0,0 +1,13 @@
+package ex3no8;
+
+public class ex3no8 {   //100p
+	public static void main(String[] args) {
+		int a = 1_000_000;
+		int b = 2_000_000;
+		
+		long c = a * b;
+		
+		System.out.println(c);
+	}
+
+}
